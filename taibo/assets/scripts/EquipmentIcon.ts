b005959cc20dc8ae79e36e30bca075e07551c5c6
vicyu1983 <vicyu@onework.io>
2024-05-02 16:01:21 +0800

    [Vic]新增hover ui動畫測試

--- taibo/assets/scripts/EquipmentIcon.ts
+++ taibo/assets/scripts/EquipmentIcon.ts
@@ -1,4 +1,4 @@
-import { _decorator, Button, Camera, Component, log, Node, NodeEventType, Vec3 } from 'cc';
+import { _decorator, Animation, Button, Camera, Component, log, Node, NodeEventType, Vec3 } from 'cc';
 import { EquipmentModel } from './EquipmentModel';
 import { Navigation } from './Navigation';
 const { ccclass, property } = _decorator;
@@ -20,16 +20,26 @@ export class EquipmentIcon extends Component {
     start() {
 
         this.model.node.on(EquipmentModel.ON_CHANGE, this.onModelChange, this);
+        this.onModelChange();
+        this.addListener();
+        this.onBtnRelease();
+    }
 
+    addListener() {
+        this.removeListener();
         this.node.on(NodeEventType.MOUSE_UP, this.onBtnClick, this);
         this.node.on(NodeEventType.MOUSE_MOVE, this.onBtnHover, this);
         this.node.on(NodeEventType.MOUSE_LEAVE, this.onBtnRelease, this);
+    }
 
-        this.onModelChange();
+    removeListener() {
+        this.node.off(NodeEventType.MOUSE_UP, this.onBtnClick, this);
+        this.node.off(NodeEventType.MOUSE_MOVE, this.onBtnHover, this);
+        this.node.off(NodeEventType.MOUSE_LEAVE, this.onBtnRelease, this);
     }
 
     onModelChange() {
-        this.node.active = this.model.showOnScreen;
+        this.node.active = this.model.getShow();
     }
 
     syncPosition() {
@@ -45,12 +55,13 @@ export class EquipmentIcon extends Component {
     }
 
     onBtnHover() {
-
+        if (this.info.active) return;
         this.info.active = true;
+        this.node.getComponent(Animation).play(this.node.getComponent(Animation).clips[0].name);
     }
 
     onBtnRelease() {
-
+        if (!this.info.active) return;
         this.info.active = false;
     }
 
